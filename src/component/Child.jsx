c7b1af12bb2dbf914b9712dcd2f96bab08c2682c
Jum-jum-maker <80886533+Jum-jum-maker@users.noreply.github.com>
2024-03-27 02:17:11 +0100

    made some changes

--- src/component/Child.jsx
+++ src/component/Child.jsx
@@ -1,14 +1,2 @@
 import React from "react";
 
-
-function Child(myProp){
-
-    return(
-        <>
-        <li>{myProp} </li>
-        </>
-    )
-
-}
-
-export default Child
--- src/component/Parent.jsx
+++ src/component/Parent.jsx
@@ -1,33 +1,38 @@
 import React from "react";
 
 import { useState } from "react";
-const url=("https://jsonplaceholder.typicode.com/todos/1")
+const url="https://jsonplaceholder.typicode.com/todos/1"
 
-const Parent= ()=>{
-  const  [data, setData]= useState([])
-  const [loading, isLoading]= useState(false)
-  const [error, setError] = useState(null) 
 
-  const getData = async()=>{
-    fetch(url).then((res)=>{
+const[data, setData]=("null")
+
+const Parent =()=>{
+  fetch(url).then((res)=>{
     console.log(res)
     return res.json()
-    console.log(res)
-    })
-    setData(getData)
+  }).then((data)=>{
+  }).catch(error=>{
+    console.log("An error occured", error)
+
+    const updateData =()=>{
+      setData(url)
+      console.log(data)
+    
+    }
+    
+  })
+
+
+  return(
+    <>
+    <h3>Hello World</h3>
+    {/* <button onClick={updateData}></button> */}
+    <p>{data.title}</p>
+    </>
+  )
 }
 
 
 
-
-
-
-
-return(
-  <>
-  <ul>{data}</ul>
-  </>
-)
-}
 
 export  default Parent
